0425f8596a637701bd61a58603016dace0856989
usmanudin <59205003+usmanudin33@users.noreply.github.com>
2021-02-04 19:37:06 +0700

    Add files via upload

---
+++ otomasi/index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	
+<title>Smart Greenhouse</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="../user.css">
+<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Raleway">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<?php include '../header.php'; include '../header.css'; ?>
+</head>
+<body>
+<?php
+ include 'auto.php';
+?>
+</body>
+</html>
